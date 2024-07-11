7574fb38cf7ef967c397c141cf29d664595c5b6e
amazzeo11 <a.mazzeo11@campus.unimib.it>
2024-07-12 00:23:32 +0200

    grado totale prova, da mettere cut

--- Prolog/mvp.pl
+++ Prolog/mvp.pl
@@ -65,13 +65,14 @@ is_zero(X) :-
    % sum_exp(V, _)
   %  .
 
-   as_monomial(E, m(C,T,V)):-
-     first_symbol(E,S),
-    integer(S),
-    C is S,
-    grado_totale(E, S),
-    T is S,
-    V is 0.
+ %  as_monomial(E, m(C,T,V)):-
+ %    first_symbol(E,S),
+  %  integer(S),
+ %   C is S,
+  %  grado_totale(E, S),
+ %   T is S,
+  %  V is 0.
+
 % Base case: if E is a number or an atom, the first symbol is E itself.
 first_symbol(E, E) :-
     (number(E); atom(E)), !.
@@ -81,5 +82,51 @@ first_symbol(E, Symbol) :-
     E =.. [_ | Args],  % Decompose the term into functor and arguments
     Args = [FirstArg | _],   % Get the first argument
     first_symbol(FirstArg, Symbol).  % Recursively find the first symbol
+
+% Predicato principale che calcola il grado totale di un monomio.
+grado_totale(Term, GradoTotale) :-
+    scomponi(Term, Termini),
+    estrai_esponenti(Termini, Esponenti),
+    somma_lista(Esponenti, GradoTotale).
+
+% Scompone il monomio nei suoi termini costituenti.
+scomponi(Term, [Term]) :-
+    atomic(Term),
+    !.
+scomponi(Term, Termini) :-
+    Term =.. [*, Term1, Term2],
+    scomponi(Term1, Termini1),
+    scomponi(Term2, Termini2),
+    append(Termini1, Termini2, Termini).
+scomponi(Term, [Term]) :-
+    Term =.. [_ | _].
+
+% Estrae gli esponenti delle variabili dai termini.
+estrai_esponenti([], []).
+estrai_esponenti([Term | Rest], [1 | Esponenti]) :-
+    atomic(Term),
+    \+ integer(Term),
+    !,
+    estrai_esponenti(Rest, Esponenti).
+estrai_esponenti([_^Exp | Rest], [Exp | Esponenti]) :-
+    !,
+    estrai_esponenti(Rest, Esponenti).
+estrai_esponenti([_ | Rest], Esponenti) :-
+    estrai_esponenti(Rest, Esponenti).
+
+% Predicato per sommare gli elementi di una lista.
+somma_lista([], 0).
+somma_lista([H | T], Somma) :-
+    somma_lista(T, Rest),
+    Somma is H + Rest.
+
+% Predicato principale per la conversione di un'espressione in monomio.
+as_monomial(E, m(C, T, V)) :-
+    first_symbol(E, S),
+    integer(S),
+    C is S,
+    grado_totale(E, GradoTotale),
+    T is GradoTotale,
+    V is 0.
 
 
